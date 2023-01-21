45716638979f809321f98332b78bc03e3926713d
Khai Sam <samkhaichi@gmail.com>
2023-01-21 11:59:44 -0800

    Updated;interface.py

--- Python/Interface.py
+++ Python/Interface.py
@@ -1,12 +1,19 @@
-import time
-
-import pandas as pd
 import csv
-import numpy as np
-import serial
-b = True
+from serial import Serial
+import time
+from os.path import isfile
 
-esp = serial.Serial(port = '/dev/cu.usbserial-0001',baudrate = 115200,timeout=.1) #change based on pot in arduino and baudrate
+# Port num of esp32 depends on OS and usb slot. Check the Arduino IDE to verify port num on computer. For Mac, use "/dev/cu.usbserial-0001".
+port_num = "COM3"
+esp32 = Serial(port=port_num, baudrate = 115200, timeout=0.1)
+
+# Create a filename in the format: res_data_{year}_{month}_{day}_test{count}.
+file_base = f"res_data_{time.strftime('%Y_%m_%d', time.gmtime())}"
+file_ext = ".csv"
+count = 1
+while isfile(file_base + f"_test{count}" + file_ext):
+    count += 1
+filename = file_base + f"_test({count})" + file_ext
 
     # b = False
     # data = esp.readline()
@@ -14,21 +21,17 @@ esp = serial.Serial(port = '/dev/cu.usbserial-0001',baudrate = 115200,timeout=.1
     # if decoded_bytes == "Starting press-sequence":
     #     b = True
     #     break
-
-
-
-# #mac pps would have to use port as "/dev/cu.usb0001" or whatever
 # print("succesfully connected at port" )
 # input = startPressSequence
-while b:
-    data = esp.readline()
-    time.sleep(0.05)
-    try:
-        decoded_bytes = float(data[0:len(data)-2].decode("utf-8"))
-        print(decoded_bytes)
-        with open("res_data.csv","a") as f:
-            writer = csv.writer(f,delimiter= ",")
-            writer.writerow([time.time(),decoded_bytes])
 
+while True:
+    time.sleep(0.05)
+    data = esp32.readline()
+    try:
+        decoded_bytes = float(data[:len(data)-2].decode("utf-8"))
+        print(decoded_bytes)
+        with open(filename, "a") as f:
+            writer = csv.writer(f, delimiter= ",")
+            writer.writerow([time.time(), decoded_bytes])
     except:
         continue
