29dc51b8b2a794ff1a31945aa0c03c1b0e18a44d
Liam McHugh <103697932+liammchugh@users.noreply.github.com>
2023-02-04 16:15:24 -0800

    relaytest
    sdf

---
+++ Arduino/Test/Relay_Test/Relay_Test.ino
@@ -0,0 +1,23 @@
+#define PIN1 23
+int count;
+float times;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(PIN1,OUTPUT);
+  count = 0;
+  times = millis();
+}
+
+void loop() {
+  if(millis() - times > 1000){
+    Serial.println(count);
+    times = millis();
+    count = count + 1;}
+    if(count % 2 == 0){
+      digitalWrite(PIN1,HIGH);
+    } else { digitalWrite(PIN1,LOW);}
+   
+  
+
+}
