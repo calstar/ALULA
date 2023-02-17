dff5b54ca85ad6ca9d79876fe7a751ce7aa8efc6
dulanyaa <dulanyac@gmail.com>
2023-02-16 20:52:06 -0800

    owo

--- Arduino/COM/Switch_Debounce/Switch_Debounce.ino
+++ Arduino/COM/Switch_Debounce/Switch_Debounce.ino
@@ -15,32 +15,33 @@ ezButton button2(22); // create ezButton object that attach to pin GIOP22;
 ezButton button3(23); // create ezButton object that attach to pin GIOP23;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   button1.setDebounceTime(DEBOUNCE_TIME); // set debounce time to 50 milliseconds
   button2.setDebounceTime(DEBOUNCE_TIME); // set debounce time to 50 milliseconds
   button3.setDebounceTime(DEBOUNCE_TIME); // set debounce time to 50 milliseconds
 }
 
 void loop() {
+  Serial.println(button1.getState());
   button1.loop(); // MUST call the loop() function first
   button2.loop(); // MUST call the loop() function first
   button3.loop(); // MUST call the loop() function first
 
-  if (button1.isPressed())
+  if (button1.getState())
     Serial.println("The button 1 is pressed");
 
-  if (button1.isReleased())
+  if (!button1.getState())
     Serial.println("The button 1 is released");
 
-  if (button2.isPressed())
+  if (button2.getState())
     Serial.println("The button 2 is pressed");
 
-  if (button2.isReleased())
+  if (!button2.getState())
     Serial.println("The button 2 is released");
 
-  if (button3.isPressed())
+  if (button3.getState())
     Serial.println("The button 3 is pressed");
 
-  if (button3.isReleased())
+  if (!button3.getState())
     Serial.println("The button 3 is released");
 }
