f455d8020ef7a3c2f04113af64a0f355310cb543
Sudhanva Kulkarni <sudhanvakulkarni@berkeley.edu>
2023-03-13 02:38:55 -0700

    added linear interpol for fluid level

--- Arduino/Test/capsens/capsens.ino
+++ Arduino/Test/capsens/capsens.ino
@@ -4,7 +4,10 @@
 int buffer[] = {0,0,0};
 long value = 0;
 float capacitance = 0;
-
+float max_capacitance = 50 ; //add this once blowdown starts
+float min_capacitance = 25; //add before fill
+float dielectric = 1;
+float initial_level = 6; //add based on placememnt of dip_tube
 
 
 void setup()
@@ -73,7 +76,9 @@ void loop(){
         count = 0;
         value = long((long)buffer[0] << 16) + ((long)buffer[1] << 8) + (long)buffer[2] - 0x800000;
         capacitance = (float)value/ 1024000.0;
+        level = (max_capacitance - min_capacitance)/dielectric + initital_level;
         Serial.println(capacitance);
+        Serial.println(level);
       } 
       }
       count++;
