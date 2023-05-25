811298d4d6145042dfc9ddeb5bbe8cf5d69edf31
Sudhanva Kulkarni <sudhanvakulkarni@berkeley.edu>
2023-05-25 11:41:21 -0400

    plotting

--- LE2_sims/new_thrust.py
+++ LE2_sims/new_thrust.py
@@ -23,6 +23,11 @@ class propellant:
 
 
 #adjust thrust calculation using hotfire attempt
+# fO = lambda x : x >= 164
+# fE = lambda x : x >= 191
+# fC = lambda x : x >= 100
+# data = pd.read_excel(r"hotfire2_2023-04-16.xlsx")
+
 ox_pres = [346.93,346.93,
                     346.93,
                     346.93,
@@ -100,12 +105,11 @@ chamber_pres = [-2.34,
 chamber_press = np.array(chamber_pres) + 4
 #implement simple grad descent(using scipy.optimize for now) to learn the best linear function of tank pressures to get chamber pressure
 
-f = lambda a: a[0]*ox_pres + a[1]*eth_pres + a[2]
+f = lambda a: a[0]*ox_pres + a[1]*eth_pres
 loss = lambda a: (np.linalg.norm((f(a) - chamber_pres)))**2
-a_star = scipy.optimize.minimize(loss,[0,0,0])
+a_star = scipy.optimize.minimize(loss,[0,0])
 a_star = a_star.x
-
-
+print(a_star)
 
 
 
@@ -114,9 +118,9 @@ a_star = a_star.x
 chamber = CEA_Obj(propName="", oxName="LOX", fuelName="C2H5OH") #initializs CEA object
 output = []
 h = 0.001
-time = 10
-n = int(10/0.001)
-time_array = np.linspace(0,10,n)
+time = 3
+n = int(time/h)
+time_array = []
 LOX_tank_volume = 0.00757
 
 ETH_tank_volume = 0.006926
@@ -128,13 +132,18 @@ print(f1+f3)
 rho_lox = 1141
 rho_eth = 798
 psi_to_Pa = 6894.76
-ETH_volume = 2.75/rho_eth #in m^3
-LOX_volume = 3.575/rho_lox
-pressure_ox = 444.7
-pressure_eth = 411.4
-P_chamber = 325
+ETH_volume = 2.5/rho_eth #in m^3
+LOX_volume = 3.5/rho_lox
+pressure_ox = 420
+pressure_eth = 420
+P_chamber = 300
 mdot = []
+pE = []
+pO = []
+mO = []
+mE = []
 for i in range(n):
+    time_array.append(i*time/n)
     pressure_ox = pressure_ox*6894.76
     pressure_eth = pressure_eth*6894.76
     P_chamber = P_chamber*6894.76
@@ -144,29 +153,35 @@ for i in range(n):
     k_lox = (Cd_LOX[0]/Cd_LOX[1])*(Cd_LOX[0]/Cd_LOX[1])
     P_inj_LOX = (k_lox*pressure_ox + P_chamber)/(k_lox + 1)
     P_inj_Eth = (k_eth*pressure_eth + P_chamber)/(k_eth + 1)
-    mdot_LOX = Cd_LOX[0]*np.sqrt(2*rho_lox*(P_inj_LOX - P_chamber))
+    mdot_LOX = Cd_LOX[0]*np.sqrt(2*rho_lox*(P_inj_LOX - P_chamber)) 
     mdot_Eth = Cd_Eth[0]*np.sqrt(2*rho_eth*(P_inj_Eth - P_chamber))
 
 
 
     #diffeq part for lox
-    pressure_ox = pressure_ox + h*(-pressure_ox)*mdot_LOX*1.4 /(rho_lox*(LOX_tank_volume - LOX_volume))
+    pressure_ox = pressure_ox - h*(pressure_ox)*mdot_LOX /(rho_lox*(LOX_tank_volume - LOX_volume))
     LOX_volume = LOX_volume - h*mdot_LOX/rho_lox
+    mO.append(LOX_volume*rho_lox)
 
 
     #diffeq part for eth
-    pressure_eth = pressure_eth + h*(-pressure_eth)*mdot_Eth*1.4 /(rho_eth*(ETH_tank_volume - ETH_volume))
-    ETH_volume = ETH_volume + h*mdot_Eth/rho_eth
-    P_chamber = a_star[0]*pressure_ox + a_star[1]*pressure_eth + a_star[2]
+    pressure_eth = pressure_eth - h*(pressure_eth)*mdot_Eth /(rho_eth*(ETH_tank_volume - ETH_volume))
+    ETH_volume = ETH_volume - h*mdot_Eth/rho_eth
+    mE.append(ETH_volume*rho_eth)
+    P_chamber = a_star[0]*pressure_ox + a_star[1]*pressure_eth 
 
     #get thrust
     pressure_ox = pressure_ox /6894.76
     pressure_eth = pressure_eth /6894.76
     P_chamber = P_chamber/6894.76
     thrust = chamber.estimate_Ambient_Isp(P_chamber,mdot_LOX/mdot_Eth,eps=1)[0]
-    thrust = 10*thrust*(mdot_Eth+mdot_LOX)
+    thrust = 9.8*thrust*(mdot_Eth+mdot_LOX)
     output.append(thrust)
     mdot.append(mdot_Eth+mdot_LOX)
+    pE.append(pressure_eth)
+    pO.append(pressure_ox)
+    if mE[len(mE)-1] <= 0 or mO[len(mO)-1] <= 0:
+        break
     
 #print(output)
 #plt.plot(time_array,output)
@@ -174,7 +189,25 @@ for i in range(n):
 thrust = chamber.estimate_Ambient_Isp(325,f3/f1,eps=1)[0]
 thrust = 10*thrust*(f3+f1)
 print(thrust)
-plt.plot(time_array,output)
+print(a_star)
+output = np.array(output)
+pE = np.array(pE)
+pO = np.array(pO)
+fig, axs = plt.subplots(3,3)
+axs[0,0].plot(time_array,output)
+axs[0,0].set_title("thrust")
+axs[1,0].plot(time_array,pE)
+axs[1,0].set_title("Press_ETH")
+axs[2,0].plot(time_array,pO)
+axs[2,0].set_title("Press_LOX")
+axs[1,1].plot(time_array,mE)
+axs[1,1].set_title("mass_ETH")
+axs[1,2].plot(time_array,mO)
+axs[1,2].set_title("mass_LOX")
+sum = 0.5*h*mdot[0] + 0.5*h*mdot[len(mdot) - 1] 
+for i in mdot[1:len(mdot) - 1]:
+    sum = sum + h*i
+print(sum)
 # plt.plot(np.linspace(0,5,(len(ox_pres))),ox_pres)
 # plt.plot(np.linspace(0,5,(len(eth_pres))),eth_pres)
 #print(output[0])
