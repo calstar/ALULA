f6c6656cfdb4e4384f8a944eede1bbb7200fac85
Ivy Sim <ivy_sim@berkeley.edu>
2023-11-13 19:30:56 -0800

    updated

--- Calibration/PT_CALIBRATION_MAIN/PT_CALIBRATION_MAIN.py
+++ Calibration/PT_CALIBRATION_MAIN/PT_CALIBRATION_MAIN.py
@@ -3,37 +3,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import deque
-import os
 import time
 from os.path import isfile
 import csv
-
-instrument_count = 8
-data_point_num = 3
-i = 1
-
-instrument_deques = [deque(maxlen=data_point_num) for _ in range(instrument_count)]
-X = [[] for _ in range(instrument_count)]
-Y = []
-
-file_base = f"calibration_{time.strftime('%Y-%m-%d', time.gmtime())}"
-file_ext = ".csv"
-test_num = 1
-
-while isfile(file_base + f"_test{test_num}" + file_ext):
-    test_num += 1
-
-filename = file_base + f"_test{test_num}" + file_ext
-
-with open(filename, "a", newline='') as f:
-    writer = csv.writer(f, delimiter=",")
-
-    row = []
-    for j in range(instrument_count):
-        row.extend([f"Reading {j + 1} X", f"Reading {j + 1} Y", "Slope (m)", "Intercept (c)"])
-    
-    writer.writerow(row)
-
+import math
 
 def read_serial():
     port_num = "/dev/cu.usbserial-0001"
@@ -73,21 +46,25 @@ def interrupt(instrument_deques):
             writer.writerow(row)
 
 def graphing(X, Y):
-    fig, axs = plt.subplots(3, 3, figsize=(12, 8))
+    num_cols = math.ceil(math.sqrt(instrument_count))
+    num_rows = math.ceil(instrument_count / num_cols)
+
+    fig, axs = plt.subplots(num_rows, num_cols, figsize=(12, 3* num_cols))
 
     val_row = []
     for j in range(instrument_count):
-        row, col = divmod(j, 3)
+        row, col = divmod(j, num_cols)
 
         x = np.array(X[j])
         y = np.array(Y)
-
         # Find line of best fit
         a, b = np.polyfit(x, y, 1)
 
         axs[row, col].scatter(x, y, color='purple')
 
-        axs[row, col].plot(x, a*x+b, color='steelblue', linestyle='--', linewidth=2, label=f'y = {b:.2f} + {a:.2f}x')
+        axs[row, col].plot(x, a * x + b, color='steelblue', linestyle='--', linewidth=2,
+                           label=f'y = {b:.6f} + {a:.6f}x')
+
         axs[row, col].set_title(f"Reading {j + 1}")
         axs[row, col].legend()
 
@@ -99,17 +76,45 @@ def graphing(X, Y):
         writer.writerow(val_row)
 
     # Remove any unused subplots
-    for j in range(instrument_count, 9):
-        row, col = divmod(j, 3)
+    for j in range(instrument_count, num_rows * num_cols):
+        row, col = divmod(j, num_cols)
         fig.delaxes(axs[row][col])
 
     plt.tight_layout()
     plt.show()
 
 if __name__ == "__main__":
+    global instrument_count
+    instrument_count = int(input("Pls input the number of instruments for calibration (INT ONLY): "))
+
+    data_point_num = 3
+
+    instrument_deques = [deque(maxlen=data_point_num) for _ in range(instrument_count)]
+    X = [[] for _ in range(instrument_count)]
+    Y = []
+
+    file_base = f"calibration_{time.strftime('%Y-%m-%d', time.gmtime())}"
+    file_ext = ".csv"
+    test_num = 1
+
+    while isfile(file_base + f"_test{test_num}" + file_ext):
+        test_num += 1
+
+    filename = file_base + f"_test{test_num}" + file_ext
+
+    with open(filename, "a", newline='') as f:
+        writer = csv.writer(f, delimiter=",")
+
+        row = []
+        for j in range(instrument_count):
+            row.extend([f"Reading {j + 1} X", f"Reading {j + 1} Y", "Slope (m)", "Intercept (c)"])
+        
+        writer.writerow(row)
+
     while True:
         read_serial()
 
         rerun = input("Would you like to run the PT calibration again? (y/n): ").strip().lower()
         if rerun != "y":
             break
+
