13b51696173fa1b02ef629ca7b4ed62d33087f9a
Ishir Garg <gargishir@gmail.com>
2023-11-17 20:43:31 -0800

    Added tc3 and tc4

--- command_acquisition/FALL2023/command_acquisition.py
+++ command_acquisition/FALL2023/command_acquisition.py
@@ -11,17 +11,17 @@ import os
 
 
 data_len = 500
-num_plots = 9
+num_plots = 11
 
 BUFFER_SIZE = 100  
 write_buffer = []
 
 deque_list = [deque(maxlen=data_len) for _ in range(num_plots + 3)]
-x, PT_O1, PT_O2, PT_E1, PT_E2, PT_C1, LC_combined, LC1, LC2, LC3, TC1, TC2 = deque_list
+x, PT_O1, PT_O2, PT_E1, PT_E2, PT_C1, LC_combined, LC1, LC2, LC3, TC1, TC2, TC3, TC4 = deque_list
 
-plot_titles = ["PT_O1", "PT_O2", "PT_E1", "PT_E2", "PT_C1", "LC Combined", "TC1", "TC2", "LCs"]
+plot_titles = ["PT_O1", "PT_O2", "PT_E1", "PT_E2", "PT_C1", "LC Combined", "TC1", "TC2", "TC3", "TC4", "LCs"]
 
-fig, axs = plt.subplots(3, 3)
+fig, axs = plt.subplots(4, 3)
 
 axs_list = axs.flatten()
 lines = [ax.plot([], [])[0] for ax in axs_list[:-1]]
@@ -59,7 +59,7 @@ def collection():
             
                 write_buffer.append(values)
 
-                if len(values) == 14:
+                if len(values) == 16:
                     x.append(float(values[0])/1000)
                     PT_O1.append(float(values[1]))
                     PT_O2.append(float(values[2]))
@@ -72,6 +72,8 @@ def collection():
                     LC_combined.append(float(values[6]) + float(values[7]) + float(values[8]))
                     TC1.append(float(values[9]))
                     TC2.append(float(values[10]))
+                    TC3.append(float(values[11]))
+                    TC4.append(float(values[12]))
 
                 if len(write_buffer) >= BUFFER_SIZE:
                         writer.writerows(write_buffer)
@@ -104,6 +106,12 @@ def animate(i):
             elif j == 7:  # For TC2
                 line.set_data(x, deque_list[11])  # TC2
                 title_data = deque_list[11][-1]
+            elif j == 8:  # For TC3
+                line.set_data(x, deque_list[12])  # TC2
+                title_data = deque_list[12][-1]
+            elif j == 9:  # For TC4
+                line.set_data(x, deque_list[13])  # TC2
+                title_data = deque_list[13][-1]
 
             ax.set_xlim(min_x, max_x)
             ax.set_title(f"{plot_titles[j]}: {title_data:.2f}", fontsize=12)
