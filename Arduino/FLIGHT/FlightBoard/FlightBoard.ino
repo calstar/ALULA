12b925b65e1a470650decb8e0d73a5e7034081d6
aarchans <118239400+aarchans@users.noreply.github.com>
2024-05-07 22:49:44 -0700

    Changed pin out for PT-X so that it works

--- Arduino/FLIGHT/FlightBoard/FlightBoard.ino
+++ Arduino/FLIGHT/FlightBoard/FlightBoard.ino
@@ -211,11 +211,11 @@ public:
 #define HX_CLK 17
 // EXTRA PIN THAT CAN BE USED: 16 (PT6)
 struct_hx711 PT_O1{ {}, HX_CLK, 15, .offset = -104.6, .slope = 0.014878509 }; //swapped w/C1 04/19
-struct_hx711 PT_O2{ {}, HX_CLK, 16, .offset = -90.9, .slope = 1.518551107 };
+struct_hx711 PT_O2{ {}, HX_CLK, 38, .offset = -90.9, .slope = 1.518551107 };
 struct_hx711 PT_E1{ {}, HX_CLK, 6, .offset = -195.1, .slope = 0.037664861 };
 struct_hx711 PT_E2{ {}, HX_CLK, 7, .offset = -200.4, .slope = 0.017309222 };
 struct_hx711 PT_C1{ {}, HX_CLK, 4, .offset = 0, .slope = 0.036179209 }; //currently broken
-struct_hx711 PT_X{ {}, HX_CLK, 5, .offset = 0, .slope = 1 }; //pt6 - extra
+struct_hx711 PT_X{ {}, HX_CLK, 39, .offset = 0, .slope = 1 }; //pt6 - extra
 
 // LOADCELLS UNUSED IN FLIGHT
 
