0c4d9faf9d33988fc9cb57a177ea5581c92abe52
Liam McHugh <103697932+liammchugh@users.noreply.github.com>
2024-05-17 15:10:20 -0700

    cleanup

--- command_acquisition/FLIGHT_2024/FLIGHT_2024_com_aq.py
+++ command_acquisition/FLIGHT_2024/FLIGHT_2024_com_aq.py
@@ -48,7 +48,8 @@ filename = file_base + f"_test{test_num}" + file_ext
 
 # for mac port_num = "/dev/cu.usbserial-0001"
 
-port_num = "/dev/cu.usbserial-0001" # CHECK YOUR PORT !!!
+# port_num = "/dev/cu.usbserial-0001" # CHECK YOUR PORT !!!
+port_num = "COM3" # CHECK YOUR PORT !!!
 esp32 = Serial(port=port_num, baudrate=115200)
 # !!! IF NO NUMBERS PRINTED ON UR TERMINAL => PRESS "EN" ON THE ESP !!!
 
